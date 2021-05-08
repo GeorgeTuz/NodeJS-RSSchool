4bf8b3b621c559965fd2a45a7dabed376e85236a
GeorgeTuz <george_tuz@mail.ru>
2021-05-08 14:05:08 +0300

    feat: add stdin as an input and output source

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const { program } = require('commander');
+const readline = require('readline');
+program.version('0.0.1');
+
+program
+    .option('-s, --shift <number>', 'integer value')
+    .option('-a, --action <string>', 'action encode/decode')
+    .option('-i, --input <file>', 'input file')
+    .option('-o, --output <file>', 'output file');
+
+program.parse(process.argv);
+const index = program.opts();
+
+if (index.shift === undefined) {
+    console.log('option shift is required')
+} else if (isNaN(index.shift)) {
+    console.log('option shift should be integer number')
+} else if (index.action === undefined) {
+    console.log('option action is required')
+} else if (!(index.action === 'encode' || index.action === 'decode')) {
+    console.log('option action should be "encode" or "decode"')
+} else if (index.input === undefined) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+    });
+    rl.on('line', function(line){
+        if (index.output === undefined) {
+            console.log(line)
+        } else {
+            let writeableStream = fs.createWriteStream(index.output);
+            writeableStream.write(line);
+        }
+    });
+} else {
+    let readableStream = fs.createReadStream(index.input, "utf8");
+    readableStream.on("data", function(chunk){
+        if (index.output === undefined) {
+            console.log(chunk)
+        } else {
+            let writeableStream = fs.createWriteStream(index.output);
+            writeableStream.write(chunk);
+        }
+    });
+}
