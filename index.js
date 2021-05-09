51fbc850a1fdd0c2949050e537c74baae50876bf
GeorgeTuz <george_tuz@mail.ru>
2021-05-09 23:40:01 +0300

    feat: use stdout for output

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ program.parse(process.argv);
 const index = program.opts();
 
 if (index.shift === undefined) {
-    console.log('option shift is required');
+    process.stdout.write('option shift is required');
 } else if (isNaN(index.shift)) {
-    console.log('option shift should be integer number');
+    process.stdout.write('option shift should be integer number');
 } else if (index.action === undefined) {
-    console.log('option action is required');
+    process.stdout.write('option action is required');
 } else if (!(index.action === 'encode' || index.action === 'decode')) {
-    console.log('option action should be "encode" or "decode"');
+    process.stdout.write('option action should be "encode" or "decode"');
 } else if (index.input === undefined) {
     const rl = readline.createInterface({
         input: process.stdin,
@@ -29,7 +29,7 @@ if (index.shift === undefined) {
     });
     rl.on('line', function(line){
         if (index.output === undefined) {
-            console.log(caesar(line, index.shift, index.action));
+            process.stdout.write(caesar(line, index.shift, index.action) + '\n');
         } else {
             let readableStream = fs.createReadStream(index.output, "utf8");
             readableStream.on("data", function(str){
@@ -42,7 +42,7 @@ if (index.shift === undefined) {
     let readableStream = fs.createReadStream(index.input, "utf8");
     readableStream.on("data", function(chunk){
         if (index.output === undefined) {
-            console.log(caesar(chunk, index.shift, index.action));
+            process.stdout.write(caesar(chunk, index.shift, index.action) + '\n');
         } else {
             let readableStream = fs.createReadStream(index.output, "utf8");
             readableStream.on("data", function(str){
